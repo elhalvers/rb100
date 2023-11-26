5052696534cc90abb192430f6a11ee3764a20331
Eric Halverson <ericleifhalverson@gmail.com>
2023-11-26 11:25:23 -0800

    Initialize repository

---
+++ the_basics/exercises/movie_titles.rb
@@ -0,0 +1,9 @@
+movie_hash = {:star_wars => 1977, :avengers_infinity_wars => 2018, :tootsie => 1982, :the_amazing_spiderman => 2012, :the_french_connection => 1971 }
+
+movie_array = [movie_hash[:star_wars], movie_hash[:avengers_infinity_wars], movie_hash[:tootsie], movie_hash[:the_amazing_spiderman], movie_hash[:the_french_connection]]
+
+puts movie_array[0]
+puts movie_array[1]
+puts movie_array[2]
+puts movie_array[3]
+puts movie_array[4]
